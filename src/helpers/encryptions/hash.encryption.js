e95ca12e96b13a9e8f5b31555ac2a57feb9f165f
fajrinasah <fajrinasah@gmail.com>
2023-07-23 11:36:35 +0700

    UPDATE: export hash & compare functions

--- src/helpers/encryptions/hash.encryption.js
+++ src/helpers/encryptions/hash.encryption.js
@@ -1,11 +1,11 @@
 import bcrypt from "bcrypt";
 
 // HASHING A VALUE
-function hash(value, salt) {
+export function hash(value, salt) {
   return bcrypt.hashSync(value, salt);
 }
 
 // COMPARING A HASHED VALUES WITH ANOTHER HASHED VALUE
-function compare(value, valueToCompare) {
+export function compare(value, valueToCompare) {
   return bcrypt.compareSync(value, valueToCompare);
 }
