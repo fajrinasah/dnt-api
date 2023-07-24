d90d86c11b63f114b75541d36db55e4325631505
fajrinasah <fajrinasah@gmail.com>
2023-07-24 12:29:15 +0700

    CHANGE: name & parent id validation schema

--- src/controllers/category/validationSchemata/categoryValidationSchema.js
+++ src/controllers/category/validationSchemata/categoryValidationSchema.js
@@ -6,8 +6,6 @@ ADD CATEGORY VALIDATION SCHEMA
 export const categoryValidationSchema = Yup.object({
   name: Yup.string()
     .required("Category name is required.")
-    .min(10, "Category name's length should be between 3 to 45 characters.")
+    .min(3, "Category name's length should be between 3 to 45 characters.")
     .max(45, "Category name's length should be between 3 to 45 characters."),
-
-  parent_id: Yup.number("Category's parent id must be a number."),
 });
