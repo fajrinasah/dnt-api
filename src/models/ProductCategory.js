bde45ee83f9297657e1e75200f907eaff75d8823
fajrinasah <fajrinasah@gmail.com>
2023-07-25 08:26:32 +0700

    CREATE: products_categories model

---
+++ src/models/ProductCategory.js
@@ -0,0 +1,27 @@
+import { DataTypes } from "sequelize";
+import db from "../database/index.js";
+
+export const ProductCategory = db.sequelize.define(
+  "product_category",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    tableName: "products_categories",
+    timestamps: false,
+  }
+);
